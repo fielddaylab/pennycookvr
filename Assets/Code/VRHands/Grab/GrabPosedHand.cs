a996c5bc2ef590059308c34ce0159c85929ca7e7
Ross Tredinnick <ross.tredinnick@gmail.com>
2024-09-20 15:55:19 -0500

    Hooked up grip animations.

--- Assets/Code/VRHands/Grab/GrabPosedHand.cs
+++ Assets/Code/VRHands/Grab/GrabPosedHand.cs
@@ -21,9 +21,28 @@ namespace FieldDay.VRHands {
 
         [NonSerialized] public bool WasGripPosed;
 
+        [NonSerialized] public int AnimLayerIndexPoint = -1;
+		[NonSerialized] public int AnimParamIndexFlex = -1;
+		[NonSerialized] public int AnimParamIndexPose = -1;
+
         private void Awake() {
             this.CacheComponent(ref CachedTransform);
             this.CacheComponent(ref CachedTracked);
+            
+            if(Animator != null) {
+				AnimLayerIndexPoint = Animator.GetLayerIndex("Point Layer");
+                AnimParamIndexFlex = Animator.StringToHash("Flex");
+			    AnimParamIndexPose = Animator.StringToHash("Pose");
+
+                Animator.SetInteger(AnimParamIndexPose, 0);
+                Animator.SetFloat("Pinch", 0f);
+			}
+        }
+
+        public void AnimateGrip(float grip) {
+            grip = Mathf.Clamp(grip, 0.0f, 0.5f);
+            Animator.SetFloat(AnimParamIndexFlex, grip);
+            Animator.SetLayerWeight(AnimLayerIndexPoint, 1.0f-grip*2f);
         }
     }
 }
