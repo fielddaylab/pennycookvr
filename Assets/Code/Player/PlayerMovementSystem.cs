29a730b88e52c7b7237a034d3ea1158c6dcad2fb
Ross Tredinnick <ross.tredinnick@gmail.com>
2024-10-04 14:29:10 -0500

    Adjusted nudge controls

--- Assets/Code/Player/PlayerMovementSystem.cs
+++ Assets/Code/Player/PlayerMovementSystem.cs
@@ -21,39 +21,37 @@ namespace Pennycook {
                 }
             }
 
-#if UNITY_EDITOR || DEVELOPMENT
-
             Vector3 flattenedLook = m_StateA.HeadLook.forward;
             flattenedLook.y = 0;
             flattenedLook.Normalize();
 
-            var notGrippedHands = default(DigitalControlStates<XRHandButtons>);
-            if (!m_StateA.LeftHand.Grabber.HeldObject) {
-                notGrippedHands |= m_StateB.LeftHand.Buttons;
-            }
-            if (!m_StateA.RightHand.Grabber.HeldObject) {
-                notGrippedHands |= m_StateB.RightHand.Buttons;
-            }
-
-            if (notGrippedHands.IsDown(XRHandButtons.TriggerButton)) {
-                if (notGrippedHands.ConsumePress(XRHandButtons.PrimaryAxisUp)) {
-                    using (var move = new PlayerRigUtils.MovementRequest(m_StateA)) {
-                        move.Translate(flattenedLook * 0.3f);
-                    }
+            if (m_StateB.LeftHand.Buttons.ConsumePress(XRHandButtons.PrimaryAxisUp)) {
+                using (var move = new PlayerRigUtils.MovementRequest(m_StateA)) {
+                    move.Translate(flattenedLook * 0.3f);
                 }
-                if (notGrippedHands.ConsumePress(XRHandButtons.PrimaryAxisDown)) {
-                    using (var move = new PlayerRigUtils.MovementRequest(m_StateA)) {
-                        move.Translate(flattenedLook * -0.3f);
-                    }
+            }
+            if (m_StateB.LeftHand.Buttons.ConsumePress(XRHandButtons.PrimaryAxisDown)) {
+                using (var move = new PlayerRigUtils.MovementRequest(m_StateA)) {
+                    move.Translate(flattenedLook * -0.3f);
                 }
             }
 
+            if (m_StateB.RightHand.Buttons.ConsumePress(XRHandButtons.PrimaryAxisUp)) {
+                using (var move = new PlayerRigUtils.MovementRequest(m_StateA)) {
+                    move.Translate(flattenedLook * 0.3f);
+                }
+            }
+            if (m_StateB.RightHand.Buttons.ConsumePress(XRHandButtons.PrimaryAxisDown)) {
+                using (var move = new PlayerRigUtils.MovementRequest(m_StateA)) {
+                    move.Translate(flattenedLook * -0.3f);
+                }
+            }
+            //PenguinNav.IsWalkable()
+            
             if (m_StateB.LeftHand.Buttons.IsDownAll(XRHandButtons.PrimaryAxisClick | XRHandButtons.TriggerButton)
                 && m_StateB.RightHand.Buttons.IsDownAll(XRHandButtons.PrimaryAxisClick | XRHandButtons.TriggerButton)) {
                 Debug.Break();
             }
-
-#endif // UNITY_EDITOR || DEVELOPMENT
         }
     }
 }
