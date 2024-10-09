b0acfc1d0a33a8a914f9ddcfb894ff2e190ebd8b
Ross <rdtredinnick@wisc.edu>
2024-10-09 17:18:44 -0500

    Updated scripting to handle needing to scan from bluff before advancing and needing to scan all 5 banded Penguins

---
+++ Assets/Code/Scripting/Actors/ScriptTabletHighlightable.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BeauUtil;
+using BeauUtil.Variants;
+using FieldDay;
+using FieldDay.Components;
+using FieldDay.Scenes;
+using FieldDay.Scripting;
+using FieldDay.Sockets;
+using Leaf.Runtime;
+using UnityEngine;
+using UnityEngine.Scripting;
+using Pennycook.Tablet;
+
+namespace Pennycook {
+	[RequireComponent(typeof(TabletHighlightable))]
+    public class ScriptTabletHighlightable : ScriptActorComponent {
+		
+        #region Inspector
+		
+		
+		#endregion // Inspector
+		
+        #region Leaf
+		private TabletHighlightable m_Highlight=null;
+        
+        private void Awake() {
+            m_Highlight = GetComponent<TabletHighlightable>();
+        }
+		
+		public bool IsIdentified() { return m_Highlight.Identified; }
+        
+        
+		[LeafMember("IsIdentified"), Preserve]
+		static public bool IsIdentified(ScriptActor actor) {
+            ScriptTabletHighlightable ss = actor.GetComponent<ScriptTabletHighlightable>();
+            if(ss != null) {
+                return ss.IsIdentified() == true;
+            }
+			return false;
+		}
+		
+        [LeafMember("IsNotIdentified"), Preserve]
+		static public bool IsNotIdentified(ScriptActor actor) {
+            ScriptTabletHighlightable ss = actor.GetComponent<ScriptTabletHighlightable>();
+            if(ss != null) {
+                return (ss.IsIdentified() == false);
+            }
+			return false;
+		}
+
+        #endregion // Leaf
+		
+    }
+}
