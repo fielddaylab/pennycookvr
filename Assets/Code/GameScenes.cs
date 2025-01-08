c3be365beb6283590da3fe6d44bbdd30c8ec2b65
Ross <rdtredinnick@wisc.edu>
2025-01-07 23:15:01 -0600

    Fixes to penguin contacts

--- Assets/Code/GameScenes.cs
+++ Assets/Code/GameScenes.cs
@@ -7,4 +7,14 @@ static public class BuildScenes {
     public static readonly SceneReference ExteriorScene_Logic = new SceneReference(0);
     // Scene 2: ExteriorScene_Nav - Assets/Scenes/ExteriorScene_Nav.unity
     public static readonly SceneReference ExteriorScene_Nav = new SceneReference(1);
+    // Scene 3: Penguins_Day1 - Assets/Scenes/Penguins_Day1.unity
+    public static readonly SceneReference Penguins_Day1 = new SceneReference(2);
+    // Scene 4: Penguins_Day2 - Assets/Scenes/Penguins_Day2.unity
+    public static readonly SceneReference Penguins_Day2 = new SceneReference(3);
+    // Scene 5: Penguins_Day3 - Assets/Scenes/Penguins_Day3.unity
+    public static readonly SceneReference Penguins_Day3 = new SceneReference(4);
+    // Scene 6: Penguins_Day4 - Assets/Scenes/Penguins_Day4.unity
+    public static readonly SceneReference Penguins_Day4 = new SceneReference(5);
+    // Scene 7: Penguins_Day5 - Assets/Scenes/Penguins_Day5.unity
+    public static readonly SceneReference Penguins_Day5 = new SceneReference(6);
 }
