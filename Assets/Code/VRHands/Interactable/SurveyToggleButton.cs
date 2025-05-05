8008b29486597d9550a1d5163fe765b10019a645
rdtredinnick <rdtredinnick@wisc.edu>
2025-05-05 13:30:06 -0500

    Adding survey json and hooking up pressing of survey buttons.

---
+++ Assets/Code/VRHands/Interactable/SurveyToggleButton.cs
@@ -0,0 +1,87 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using BeauUtil;
+using BeauRoutine;
+using FieldDay;
+using FieldDay.Audio;
+using FieldDay.Components;
+using FieldDay.Scripting;
+using UnityEngine;
+
+
+namespace Pennycook {
+    public class SurveyToggleButton : BatchedComponent {
+		
+        #region Inspector
+        [Required]
+		public TriggerListener Detector;
+		
+		[SerializeField]
+		Texture2D FilledGraphic;
+		
+		[SerializeField]
+		Texture2D UnfilledGraphic;
+		
+        #endregion // Inspector
+		
+		[NonSerialized] public bool WasPressed = false;
+
+        [NonSerialized] public bool IsPressed = false;
+		
+		//private Rigidbody CachedRB;
+		private MeshRenderer CachedMR;
+		
+        //public readonly CastableEvent<Button> OnPressed = new CastableEvent<Button>();
+		
+		[AudioEventRef]
+		public StringHash32 ButtonSound;
+		
+		private void Awake() {
+
+			Detector.onTriggerEnter.AddListener(ButtonTrigger);
+
+			CachedMR = GetComponent<MeshRenderer>();
+
+        }
+
+		public void Untoggle() {
+			IsPressed = false;
+		}
+		
+		public void ButtonTrigger(Collider c) {
+
+			/*if(CachedRB != null) {
+				CachedRB.detectCollisions = false;
+			}*/
+			
+			if(!WasPressed) {
+				WasPressed = true;
+			}
+			
+			IsPressed = !IsPressed;
+
+			//Sfx.OneShot("button-click", transform.position);
+			
+			if(!IsPressed) {
+				//switch to unfilled graphic
+				CachedMR.material.mainTexture = UnfilledGraphic;
+			} else {
+				//switch to filled graphic
+				CachedMR.material.mainTexture = FilledGraphic;
+			}
+
+			Sfx.Play(ButtonSound, transform);
+			
+			//haptics...
+			//todo - optimize
+			/*VRInputState data = Find.State<VRInputState>();
+			if(c.gameObject.name.StartsWith("Left")) {
+				data.LeftHand.HapticImpulse = 0.25f;
+			} else if(c.gameObject.name.StartsWith("Right")) {
+				data.RightHand.HapticImpulse = 0.25f;
+			}*/
+
+		}
+    }
+}
