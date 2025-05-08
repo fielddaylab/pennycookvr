b8be489c6e2cd7d22d99b0befe5957ec9de63b65
rdtredinnick <rdtredinnick@wisc.edu>
2025-05-07 22:36:52 -0500

    Working on optimizations and progression through survey questions.

--- Assets/Code/Survey/LoadSurvey.cs
+++ Assets/Code/Survey/LoadSurvey.cs
@@ -22,6 +22,7 @@ namespace Pennycook {
         
         int CurrentSurvey = 0;
         int CurrentPage = 0;
+        int CurrentQuestion = 0;
 
         static string[] DefaultResponse = new string[7] {"Strongly Disagree", "Disagree", "Somewhat Disagree", "Neutral", "Somewhat Agree", "Agree", "Strongly Agree"};
 
@@ -41,50 +42,72 @@ namespace Pennycook {
             SurveyLikert2.IsCustom = isCustom;
 
             if(isCustom) {
-                SurveyLikert1.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[0].Prompt;
-                SurveyLikert2.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[1].Prompt;
+                SurveyLikert1.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion].Prompt;
+                SurveyLikert2.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion+1].Prompt;
                 //load in custom responses...
-                for(int i = 0; i < OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[0].Responses.Length; ++i) {
-                    SurveyLikert1.SetCustomResponse(OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[0].Responses[i], i);
+                for(int i = 0; i < OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion].Responses.Length; ++i) {
+                    SurveyLikert1.SetCustomResponse(OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion].Responses[i], i);
                 }
 
-                for(int i = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[0].Responses.Length; i < 7; ++i) {
+                for(int i = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion].Responses.Length; i < 7; ++i) {
                     SurveyLikert1.SetEnabled(false, i);
                 }
 
-                for(int i = 0; i < OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[1].Responses.Length; ++i) {
-                    SurveyLikert2.SetCustomResponse(OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[1].Responses[i], i);
+                for(int i = 0; i < OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion+1].Responses.Length; ++i) {
+                    SurveyLikert2.SetCustomResponse(OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion+1].Responses[i], i);
                 }
 
-                for(int i = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[1].Responses.Length; i < 7; ++i) {
+                for(int i = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion+1].Responses.Length; i < 7; ++i) {
                     SurveyLikert2.SetEnabled(false, i);
                 }
             } else {
-                SurveyLikert1.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[0].Prompt;
-                SurveyLikert2.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[1].Prompt;
-
+                SurveyLikert1.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion].Prompt;
                 for(int i = 0; i < DefaultResponse.Length; ++i) {
                     SurveyLikert1.SetCustomResponse(DefaultResponse[i], i);
                     SurveyLikert1.SetEnabled(true, i);
                 }
 
-                for(int i = 0; i < DefaultResponse.Length; ++i) {
-                    SurveyLikert2.SetCustomResponse(DefaultResponse[i], i);
-                    SurveyLikert2.SetEnabled(true, i);
+                if(CurrentQuestion+1 < OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions.Length) {
+                    SurveyLikert2.gameObject.SetActive(true);
+                    SurveyLikert2.GetQuestion().text = OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions[CurrentQuestion+1].Prompt;
+                    for(int i = 0; i < DefaultResponse.Length; ++i) {
+                        SurveyLikert2.SetCustomResponse(DefaultResponse[i], i);
+                        SurveyLikert2.SetEnabled(true, i);
+                    }
+                } else {
+                    SurveyLikert2.gameObject.SetActive(false);
                 }
+
             }
         }
+		/*void Update()
+		{
+			if(UnityEngine.Input.GetKeyDown("q")) {
+				NextQuestions();
+			}
+		}*/
 
         public void NextQuestions()
         {
-            if(CurrentPage == OpenGameDataSurvey.Surveys[CurrentSurvey].Pages.Length-1) {
-                CurrentSurvey++;
-                CurrentPage = 0;
-            } else {
+            CurrentQuestion+=2;
+            if(CurrentQuestion >= OpenGameDataSurvey.Surveys[CurrentSurvey].Pages[CurrentPage].Questions.Length) {
                 CurrentPage++;
-            }
+                CurrentQuestion = 0;
+                if(CurrentPage >= OpenGameDataSurvey.Surveys[CurrentSurvey].Pages.Length) {
+                    CurrentSurvey++;
+                    CurrentPage = 0;
+                    CurrentQuestion = 0;
+                }
+            } 
 
-            LoadLikertQuestions(CurrentSurvey == 0);
+            //Debug.Log(CurrentSurvey + " " + CurrentPage + " " + CurrentQuestion);
+
+            if(CurrentSurvey < OpenGameDataSurvey.Surveys.Length) {
+                LoadLikertQuestions(CurrentSurvey == 0);
+            } else {
+                //send in the data...and hide survey.
+                gameObject.SetActive(false);
+            }
         }
     }
 }
