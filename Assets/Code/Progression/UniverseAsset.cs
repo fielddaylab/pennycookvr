ee3979ce483d8eb9d430be5d0be5d985fa2b41d2
Ross <rdtredinnick@wisc.edu>
2025-06-26 17:12:02 -0500

    More optimizations and adjustments

--- Assets/Code/Progression/UniverseAsset.cs
+++ Assets/Code/Progression/UniverseAsset.cs
@@ -36,7 +36,7 @@ namespace Pennycook {
         static private IEnumerator Leaf_LoadNextDay([BindThread] ScriptThread thread) {
             var state = Find.State<PlayerProgressState>();
             VRGame.Events.Dispatch(GameEvents.DayCompleted, state.DayIndex);
-            if (state.DayIndex + 1 == 5) {
+            if (state.DayIndex + 1 == 6) {
                 state.DayIndex = 0;
                 return Leaf_LoadDay(thread, state.DayIndex);
             } else {
